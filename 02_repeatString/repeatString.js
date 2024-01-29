aafc1e2e637963e833f143ee294bc9a443572904
digweed67 <124364643+digweed67@users.noreply.github.com>
2024-01-29 14:17:23 +0100

    complete and corrected

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+function repeatString(string, num) {
+    let result = "";
+    for (let i = 0; i < num; i++) {
+        result += string;
+    }
+    return result;
+}
 
-};
+// Example usage
+let repeatedString = repeatString('Hello, ', 3);
+console.log(repeatedString);  // Output: Hello, Hello, Hello,
 
-// Do not edit below this line
-module.exports = repeatString;
+
+
+ 
+
