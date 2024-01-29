2aff7fe5fe887712c3cb117208e22ffed1b0ea6b
digweed67 <124364643+digweed67@users.noreply.github.com>
2024-01-29 14:31:42 +0100

    add comments and explanations

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -11,6 +11,24 @@ let repeatedString = repeatString('Hello, ', 3);
 console.log(repeatedString);  // Output: Hello, Hello, Hello,
 
 
+/* Let's go through an example with word = 'Hello, ' and the loop running three times:
 
+Iteration 1: string is an empty string, and word is appended to it: string = '' + 'Hello, ', resulting in string = 'Hello, '.
+
+Iteration 2: string now contains 'Hello, ', and word is appended to it again: string = 'Hello, ' + 'Hello, ', resulting in string = 'Hello, Hello, '.
+
+Iteration 3: string now contains 'Hello, Hello, ', and word is appended once more: string = 'Hello, Hello, ' + 'Hello, ', resulting in string = 'Hello, Hello, Hello, '.
+
+
+if we used string = word, it would overwrite it not add it each time (it would say hello no hellohellohello  */
  
+const repeatString = function (word, times) {
+    if (times < 0) return "ERROR";
+    let string = "";
+    for (let i = 0; i < times; i++) {
+      string += word;
+    }
+    return string;
+  };
+
 
