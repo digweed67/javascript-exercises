2e7b3bb258a54321243236a4caa068356fd1d423
digweed67 <124364643+digweed67@users.noreply.github.com>
2024-01-31 13:54:57 +0100

    finsih task

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let string = " ";
+    const myArray = string.split("").reverse().join(" ");
+    console.log(myArray);
+
+
 
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
