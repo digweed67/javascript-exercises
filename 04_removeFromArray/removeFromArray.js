e164d6b717c7c52234f0787e74f643f41b241330
digweed67 <124364643+digweed67@users.noreply.github.com>
2024-02-02 12:51:33 +0100

    find solution without using rest parameter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+function removeFromArray (elementToRemove, myArray) {
+    
+    let indexToRemove = myArray.indexOf(elementToRemove);
 
-};
+    if (indexToRemove !== -1) {
+        myArray.splice(indexToRemove, 1);
+        console.log("modified array:", myArray);
+    } else {
+        console.log("This item doesn't exist. Try again");
+    }
+    
+
+
+}
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
