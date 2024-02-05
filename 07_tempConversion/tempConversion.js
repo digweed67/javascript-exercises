fdb00fa6ffbd5beb1b989ba746dc9efefa7c0c05
digweed67 <124364643+digweed67@users.noreply.github.com>
2024-02-05 14:51:35 +0100

    less clean code but same result

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let celsius = (temperature - 32) / 1.8;
+  let roundedNumberC = Math.round(celsius*10)/10;
+  return roundedNumberC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let fahrenheit = (temperature*1.8) +32;
+  let roundedNumberF = Math.round(fahrenheit*10)/10; 
+  return roundedNumberF
 };
 
 // Do not edit below this line
